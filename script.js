f0ade33286f53aa8f5b061976cf1fe08581e839b
Swati_412 <swatipasi@gmail.com>
2021-02-01 13:24:15 +0530

    after random question with random values

--- script.js
+++ script.js
@@ -14,12 +14,16 @@ let currentQuestion;
 let availableQuestion = [];
 let availableOptions = [];
 
+function shuffleArray(array) {
+   for (var i = array.length - 1; i > 0; i--) {
+       var j = Math.floor(Math.random() * (i + 1));
 
-function setAvailableQuestions(){
-  const totalQuestion = questions.length ;
-  for(let i=0; i<totalQuestion; i++){
-    availableQuestion.push(questions[i]);
-  }
+       var temp = array[i];
+       array[i] = array[j];
+       array[j] = temp;
+   }
+
+   return array;
 }
 
 //if start quiz is clicked
@@ -36,7 +40,6 @@ exit_btn.onclick = function(){
 continue_btn.onclick = function(){
   info_box.classList.remove("activeInfo"); //hide info box
   quiz_box.classList.add("activeQuiz"); //show quiz box
-  setAvailableQuestions();
   getNewQuestion();
   queCounter(1);
   startTimer(15);
@@ -62,46 +65,87 @@ quit_quiz.onclick = function(){
 
 //if correct option is clicked
 function next(){
-  if(que_count < 2){
+  if (que_count < 2) {
     que_count+=1;
     que_numb+=1;
     getNewQuestion();
     queCounter(que_numb);
     timeOff.textContent = "Time Left";
-  }else{
-      //console.log("ques completed");
+  } else {
       clearInterval(counter);
       clearInterval(counterLine);
-      //var t = document.querySelector(".timer_sec");
-      //console.log("timeValue", 15 - t.textContent);
       generateResults();
   }
 }
 
 //get new // QUESTION:
 function getNewQuestion() {
+
+  var operators = [{
+        sign: "+",
+        method: function(a,b){ return parseFloat((a + b).toFixed(2)); }
+    },
+    {
+        sign: "-",
+        method: function(a,b){ return parseFloat((a - b).toFixed(2)); }
+    },
+    {
+        sign: "*",
+        method: function(a,b){ return parseFloat((a * b).toFixed(2)); }
+    },
+    {
+        sign: "/",
+        method: function(a,b){ return parseFloat((a / b).toFixed(2)); }
+    }
+  ];
+  var numAll = [0, 1, 2 , 3, 4, 5, 6, 7, 8, 9];
+
+  var selectedOperator = Math.floor(Math.random()*operators.length);
+  var rnum1 = Math.floor(Math.random()*(numAll.length));
+  var num1= numAll[rnum1];
+  numAll.splice(rnum1, 1);
+  var rnum2 = Math.floor(Math.random()*(numAll.length));
+  var num2= numAll[rnum2];
+  numAll.splice(rnum2, 1);
+  operators[selectedOperator].sign;
+  operators[selectedOperator].method(num1, num2);
+  var optionArr = [6, 2, 9];
+
+  if (optionArr.includes(operators[selectedOperator].method(num1, num2))) {
+    optionArr.push(10);
+  } else {
+    optionArr.push(operators[selectedOperator].method(num1, num2));
+  }
+
+  var randomOptions = shuffleArray(optionArr);
+  var questions = {
+      question: `what is ${num1} ${operators[selectedOperator].sign} ${num2} ?`,
+      answer: operators[selectedOperator].method(num1, num2),
+      options: [
+        optionArr[0],
+        optionArr[1],
+        optionArr[2],
+        optionArr[3]
+      ]
+  }
+
   //set ques textContent //random ques
   const que_text = document.querySelector(".que_text");
-  const questionIndex = availableQuestion[Math.floor(Math.random() * availableQuestion.length)];
-  //console.log("questionIndex",questionIndex);
-  currentQuestion = questionIndex;
-  var que_tag = '<span>'+ (questionCounter +1) + ". " + currentQuestion.question+'</span>';
-  // get index of currentQuestion and remove it
-  var index1 = availableQuestion.indexOf(questionIndex);
-  //remove
-  availableQuestion.splice(index1, 1);
+
+  var que_tag = '<span>'+ (questionCounter +1) + ". " + questions.question+'</span>';
   // create options in html
-  var option_tag = '<div class="option"><span>'+ currentQuestion.options[0] +'</span></div>'
-                  + '<div class="option"><span>'+currentQuestion.options[1] +'</span></div>'
-                  + '<div class="option"><span>'+ currentQuestion.options[2] +'</span></div>'
-                  +'<div class="option"><span>'+ currentQuestion.options[3] +'</span></div>';
+  var option_tag = '<div class="option"><span>'+ questions.options[0] +'</span></div>'
+                  + '<div class="option"><span>'+questions.options[1] +'</span></div>'
+                  + '<div class="option"><span>'+ questions.options[2] +'</span></div>'
+                  +'<div class="option"><span>'+ questions.options[3] +'</span></div>';
 
   que_text.innerHTML = que_tag;
   option_list.innerHTML = option_tag;
-  var correctans = currentQuestion.answer;
+  var correctans = questions.answer;
   const option = option_list.querySelectorAll(".option");
+
   for (var i = 0; i < option.length; i++) {
-    option[i].setAttribute('onClick','optionSelected(this,'+currentQuestion.answer+')');
+    option[i].setAttribute('onClick','optionSelected(this,'+questions.answer+')');
   }
   questionCounter+=1;
 }
@@ -112,13 +156,11 @@ var crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
 
 function optionSelected(answer, correctAns){
-  //console.log("correctans", correctAns);
   var userAns = answer.textContent;
   var allOptions = option_list.children.length;
-  if(userAns == correctAns){
-    userScore+=1;
-    //console.log("userScore", userScore);
 
+  if (userAns == correctAns) {
+    userScore+=1;
     answer.classList.add("correct");
     answer.insertAdjacentHTML("beforeend", tickIcon);
 
@@ -127,7 +169,7 @@ function optionSelected(answer, correctAns){
     }
     next();
 
-  }else{
+  } else {
     answer.classList.add("incorrect");
     answer.insertAdjacentHTML("beforeend", crossIcon);
     info_box.classList.remove("activeInfo"); //hide info box
@@ -135,9 +177,6 @@ function optionSelected(answer, correctAns){
     gameOver.classList.add("activeOver");
     clearInterval(counter);
     clearInterval(counterLine);
-    //var t = document.querySelector(".timer_sec");
-    //console.log("timeValue", 15 - t.textContent);
-
   }
 }
 
@@ -147,11 +186,11 @@ function showResultBox(a,b) {
   quiz_box.classList.remove("activeQuiz"); //hide quiz box
   result_box.classList.add("activeResult"); //show result box
   const scoreText =  result_box.querySelector(".score_text");
-  // console.log("userScore", userScore);
-  if(a === "YOU"){
+
+  if (a === "YOU") {
     let scoreTag = '<span>and the winner is <p>'+ 'YOU' +'</p>with time<p>'+b+'</p>s</span>';
     scoreText.innerHTML = scoreTag;
-  }else{
+  } else {
     let scoreTag = '<span>The winner is <p>Player</p><p>'+a+'</p><p>with time</p><p>'+b+'</p>s</span>';
     scoreText.innerHTML = scoreTag;
   }
@@ -163,16 +202,16 @@ function startTimer(time){
   function timer(){
     timeCount.textContent = time;
     time-=1;
-    if(time < 9){
+
+    if (time < 9) {
       let addZero = timeCount.textContent;
       timeCount.textContent = "0"+ addZero;
     }
-    if(time < 0){
+    if (time < 0) {
       clearInterval(counter);
       timeCount.textContent = "00";
       timeOff.textContent = "Time Off";
       var t = document.querySelector(".timer_sec");
-      //console.log("timeValue", t.textContent);
       generateResults();
     }
   }
@@ -183,7 +222,7 @@ function startTimerLine(time){
   function timer(){
     time+=1;
     timeLine.style.width = time + "px";
-    if(time > 549){
+    if (time > 549) {
       clearInterval(counterLine);
     }
   }
@@ -200,29 +239,25 @@ function queCounter(index){
 //generate Leaderboard
 function generateResults(){
     var t = document.querySelector(".timer_sec");
-    //console.log("timeValue", 15 - t.textContent);
     clearInterval(counter);
     clearInterval(counterLine);
     info_box.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
 
     let users = {};
-    for(let i=0;i<5;i++)
+    for (let i=0;i<5;i++)
       users[i] = parseFloat((Math.random() * 14 ).toFixed(2));
     users[5] = (15 - t.textContent);
-    //console.log("mytime", (15-t.textContent));
     let usersArray = Object.entries(users).sort((a,b)=>{return a[1]-b[1]})
-    //console.log("users",usersArray);
     let showResults = document.querySelector(".showResults");
     showResults.classList.add("activeScore");
 
         for (var j = 1; j<7;j++) {
-            if(usersArray[j-1][1] == (15 - t.textContent)){
+            if (usersArray[j-1][1] == (15 - t.textContent)) {
               let div = document.createElement('div');
               div.innerHTML = "YOU" + " : " + usersArray[j-1][1];
               showResults.appendChild(div);
-            }
-            else{
+            } else {
               let div = document.createElement('div');
               div.innerHTML = "PLAYER" + usersArray[j-1][0] + " : " + usersArray[j-1][1];
               showResults.appendChild(div);
@@ -234,20 +269,20 @@ function generateResults(){
           att.value = "winner";                           // Setting the value of the class as winner
           winnerFinal.setAttributeNode(att);
           showResults.appendChild(winnerFinal);
-          if(usersArray[0][1] == (15 - t.textContent)){
+
+          if (usersArray[0][1] == (15 - t.textContent)) {
             a = "YOU";
             b = usersArray[0][1];
-          }else{
+          } else {
               a = usersArray[0][0];
               b = usersArray[0][1];
           }
+
           showResults.childNodes[3].style.color="green";
 
           var winnerButton = document.querySelector(".winner");
-          //console.log("winnerButton", winnerButton);
           winnerButton.onclick = function(){
             showResults.classList.remove("activeScore"); //hide Leaderboard box
-            //console.log("winner clicked");
             showResultBox(a,b);
           };
 
